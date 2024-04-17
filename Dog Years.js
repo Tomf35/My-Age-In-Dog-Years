99792b6bc9abf7060d08c2dfabee653193d09ed9
Tomf35 <132650968+Tomf35@users.noreply.github.com>
2024-04-17 19:30:00 +0100

    Create Dog Years.js

---
+++ Dog Years.js
@@ -0,0 +1,13 @@
+// My age in years
+const myAge = 23;
+// Early years of a dogs life in human terms
+let earlyYears = 2;
+earlyYears *= 10.5;
+// After 2 the aging is different
+let laterYears = myAge - 2;
+// Multiplying to work out dog years for laterYears
+laterYears *= 4;
+// Working out the total in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Tom Fuller'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
